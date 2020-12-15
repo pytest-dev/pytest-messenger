d83aab41e85cbbdec292677f94af5f293c00c77f
Arseniy Antonov <arseny.antonov@gmail.com>
2020-12-15 15:13:30 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/pytest-dev/pytest-slack',
-    version='2.3.0',
+    version='2.3.1',
     zip_safe=False,
     entry_points={
         'pytest11': [
